220bda1a170048e139092521392e9e1e99ca2d25
spezoa <108841331+spezoa@users.noreply.github.com>
2025-08-30 05:33:19 -0400

    Update and rename next.config.mjs to next.config.js

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // experimental: {},
+
+  // Redirección opcional:
+  // async redirects() {
+  //   return [{ source: "/", destination: "/newui", permanent: false }];
+  // },
+
+  // Rewrites opcionales:
+  // async rewrites() {
+  //   return [
+  //     { source: "/tickets", destination: "/newui/tickets" },
+  //     { source: "/tickets/new", destination: "/newui/tickets/new" },
+  //   ];
+  // },
+};
+
+module.exports = nextConfig;
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      { source: "/", destination: "/newui", permanent: false },
-    ];
-  },
-};
-module.exports = nextConfig;
