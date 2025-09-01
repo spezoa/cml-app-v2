8b5ceff3882ff960c16beb3febb004e7a39aa03c
Sebastian Pezoa <sebastian.pezoa@versiliasolutions.com>
2025-09-01 11:41:58 -0500

    Se modifica shell file para poder trabajar desde barrel

--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,5 @@
 // components/index.ts
-export { Shell } from './layout/Shell';
+export { Shell } from './Shell';
 
 // Si te sirve, re-exporta también componentes de features
 export { default as TicketsList } from './tickets/TicketsList';
