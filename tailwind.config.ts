809db8c4f093376b06a042b9e03665431766387e
Sebastian Pezoa <sebastian.pezoa@versiliasolutions.com>
2025-09-01 13:46:17 -0500

    Se corrige dark mode en tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,3 +6,11 @@ export default {
   theme: { extend: {} },
   plugins: [],
 } satisfies Config;
+
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class',
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: { extend: {} },
+  plugins: [],
+};
